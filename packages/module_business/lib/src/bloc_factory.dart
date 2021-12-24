0e428d637b62e45500339430e5279b38166088e9
Шарипов Наиль <nikorurk@icloud.com>
2021-12-24 17:40:10 +0500

    1. serviceLocator has been introduced 2. DummyService has been registered inside serviceLocator in service_provider.dart file. 3. MainBloc has been registered inside serviceLocator in bloc_factory.dart file

--- packages/module_business/lib/src/bloc_factory.dart
+++ packages/module_business/lib/src/bloc_factory.dart
@@ -17,8 +17,17 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:module_data/module_data.dart';
 
+import '../module_business.dart';
+
+final sl = GetIt.instance;
+ void initialize() {
+   // ServiceProvider.instance.initialize();
+   sl.registerFactory<MainBloc>(() => MainBloc(
+     userService: GetIt.instance.get<UserService>()
+   ));
+ }
 @InjectableInit()
 void initializeBlocs() {
   initializeServices();
-  $initGetIt(GetIt.I);
+  // $initGetIt(GetIt.I);
 }
--- packages/module_data/lib/src/service_provider.dart
+++ packages/module_data/lib/src/service_provider.dart
@@ -7,13 +7,20 @@
 // static final instance = ServiceProvider();
 //
 // void initialize() {
-//   _getIt.registerLazySingleton<HealthService>(() => DummyService());
+//   _getIt.registerLazySingleton<UserService>(() => DummyService());
 // }
 //
 // }
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import '../module_data.dart';
 import 'service_provider.config.dart';
+
+final sl = GetIt.instance;
+void initialize() {
+  sl.registerLazySingleton<UserService>(() => DummyService());
+}
+
 
 @InjectableInit()
 void initializeServices() => $initGetIt(GetIt.I);
